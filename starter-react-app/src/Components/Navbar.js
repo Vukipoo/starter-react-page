18c94c534f43947c9200aa22ea14826e12b41ef0
Vukipoo <perovicv515@gmail.com>
2023-05-21 00:30:54 -0700

    finish little proj

---
+++ starter-react-app/src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
